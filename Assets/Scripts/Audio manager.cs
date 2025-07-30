98e6ad38e3869efcf0a2a3863fad039be063736f
ivana-antunovic <ivana.antunovic.ri@gmail.com>
2025-07-30 20:42:34 +0200

    Main Menu- UI, Audio Manager

---
+++ Assets/Scripts/Audio manager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioClip mainMenuMusic;
+    public AudioClip gameMusic;
+
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject); 
+        audioSource = GetComponent<AudioSource>();
+        SceneManager.sceneLoaded += OnSceneLoaded; 
+    }
+
+    private void Start()
+    {
+        PlayMusic(mainMenuMusic);
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name == "MainMenu")
+        {
+            PlayMusic(mainMenuMusic);
+        }
+        else if (scene.name == "Game") 
+        {
+            PlayMusic(gameMusic);
+        }
+    }
+
+    private void PlayMusic(AudioClip clip)
+    {
+        if (clip == null || audioSource == null) return;
+
+        
+
+        audioSource.Stop();
+        audioSource.clip = clip;
+        audioSource.loop = true;
+        audioSource.Play();
+    }
+}
